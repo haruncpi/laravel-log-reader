f91c9d17b8b5fbb13696d19d9142e3d6b83fe791
Holbi <AkkuFresh@users.noreply.github.com>
2020-02-07 21:02:04 +0100

    Laravel Log Reader Language Lines HU

---
+++ resources/lang/hu/laravel-log-reader.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel Log Reader Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'clear_all' => "Összes törlése",
+    'doc' => "Doku",
+    'goto_admin_panel' => "Vissza az Admin Panelra",
+    'package' => 'Laravel Naplóolvasó',
+    'title' => 'Naplóolvasó',
+    'no_log' => 'Nincs beolvasható naplófájl',
+    'no_log_by_date' => 'Nincs beolvasható naplófájl a választott naphoz: ',
+    'showing_logs' => 'Listázott napló',
+    'sure' => 'Biztos vagy benne?',
+];
