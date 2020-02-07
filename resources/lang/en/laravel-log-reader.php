cd930bdc19ebad8f44c7e8a3b332a27cf49bb3aa
Holbi <AkkuFresh@users.noreply.github.com>
2020-02-07 21:00:29 +0100

    Laravel Log Reader Language Lines - EN

---
+++ resources/lang/en/laravel-log-reader.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel Log Reader Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'clear_all' => "Clear All",
+    'doc' => "DOC",
+    'goto_admin_panel' => "Goto Admin Panel",
+    'package' => 'Laravel Log Reader',
+    'title' => 'Log Reader',
+    'no_log' => 'No log available',
+    'no_log_by_date' => 'No log file found with selected date ',
+    'showing_logs' => 'Showing Logs',
+    'sure' => 'Are you sure?',
+];
