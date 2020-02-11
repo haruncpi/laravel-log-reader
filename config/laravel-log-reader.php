f237a1c82c36653d45340a3b488d025f287cddc4
Holbi <AkkuFresh@users.noreply.github.com>
2020-02-11 11:25:29 +0100

    Update laravel-log-reader.php

--- config/laravel-log-reader.php
+++ config/laravel-log-reader.php
@@ -3,5 +3,5 @@
 return [
     'api_route_path' => 'admin/api/log-reader',
     'view_route_path' => 'admin/log-reader',
-    'admin_panel_routh_name' => 'dashboard',
+    'admin_panel_route_name' => 'dashboard',
 ];
