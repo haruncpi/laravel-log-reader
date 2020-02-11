206752151b67e39c0918e312e17443071aeffac6
Holbi <AkkuFresh@users.noreply.github.com>
2020-02-11 11:24:27 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-Route::group(['namespace' => 'Haruncpi\LaravelLogReader\Controllers', 'middleware' => ['web','auth']], function () {
+Route::group(['namespace' => 'Successdev\LaravelLogReader\Controllers', 'middleware' => ['web','auth']], function () {
 
     Route::get(config('laravel-log-reader.view_route_path'), 'LogReaderController@getIndex');
     Route::post(config('laravel-log-reader.view_route_path'), 'LogReaderController@postDelete');
